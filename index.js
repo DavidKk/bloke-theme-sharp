445358b22c24b28253a47a88bdaee9e38432e22e
davidjones <qowera@qq.com>
2017-03-23 00:35:15 +0800

    删除多余文件

--- index.js
+++ index.js
@@ -1,17 +1,1 @@
-const fs   = require('fs');
-const path = require('path');
-
-let filename = '/bloke.theme.config.js';
-let es5 = path.join(__dirname, './disttheme/', filename);
-let es6 = path.join(__dirname, './src/theme/', filename);
-
-if (fs.existsSync(es5)) {
-  module.exports = require(es5).default;
-}
-else {
-  require('babel-register');
-  require('babel-preset-es2015');
-  require('babel-preset-stage-0');
-
-  module.exports = require(es6).default;
-}
+module.exports = require('./dist/theme/bloke.theme.config.js').default;
